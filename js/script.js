b01043470e002a5768eb7a5fcc8393b5619c41d7
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-17 15:00:02 +0100

    array + push + ordina

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// alert();
+//1. Chiedere all’utente il cognome
+//2. inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’.
+//3. stampa la lista ordinata alfabeticamente (occhio alle maiuscole, se volete potete convertire il nome prima inserirlo).
+//4. Scrivi anche la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova
+
+//scrivo l'arrey con la lista dei cognomi
+var listaCognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
+// inserisco voce ad array con push attraverso inserimento dato con prompt
+listaCognomi.push(prompt("inserire cognome"));
+// ordino alfabeticamente la lista
+listaCognomi.sort();
+console.log(listaCognomi);
