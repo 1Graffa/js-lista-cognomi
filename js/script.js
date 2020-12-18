1481460ccdcfacb4be5ddffb97ac30ac7dacfd3b
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-18 10:12:51 +0100

    ieri

--- js/script.js
+++ js/script.js
@@ -4,12 +4,20 @@
 //3. stampa la lista ordinata alfabeticamente (occhio alle maiuscole, se volete potete convertire il nome prima inserirlo).
 //4. Scrivi anche la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova
 
-//scrivo l'arrey con la lista dei cognomi
-var listaCognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
+//scrivo le variabili
+var listaCognomi, inputCognome, toUp, toLow;
+// arrey con la lista dei cognomi
+listaCognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
 // inserisco voce ad array con push attraverso inserimento dato con prompt
-listaCognomi.push(prompt("inserire cognome"));
-// ordino alfabeticamente la lista
+inputCognome = prompt("inserire cognome")
+// ordino alfabeticamente la lista e correggo le maiuscole e minuscole
+toUp = inputCognome.charAt(0).toUpperCase();
+toLow = inputCognome.substring(1).toLowerCase();
+inputCognome = toUp + toLow;
+listaCognomi.push(inputCognome);
 listaCognomi.sort();
 console.log(listaCognomi);
-// stampo con posizione
+// la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array)
+//indexOf(inputCognome) + 1)
+// stampo
 document.getElementById('lista').innerHTML = listaCognomi;
